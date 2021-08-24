6ef26eb6347fccf9501abb8092e17043935e8a1c
jrguedelha <jrguedelha@gmail.com>
2021-08-24 15:01:10 -0300

    Criado MovieController.cs com os metodos CRUD

--- BlockbusterAPI/Controllers/MovieController.cs
+++ BlockbusterAPI/Controllers/MovieController.cs
@@ -1,15 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlockbusterAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlockbusterAPI.Controllers
 {
-     [ApiController]
+    [ApiController]
     [Route("[controller]")]
     public class MovieController : Controller
     {
@@ -24,8 +21,8 @@ namespace BlockbusterAPI.Controllers
         public JsonResult Get()
         {
             string query = @"
-                            select Id, Title, Release, Active
-                            from dbo.Movie
+                            SELECT Id, Title, Release, Active
+                            FROM dbo.Movie
                             ";
             DataTable table = new DataTable();
             string dataSource = _configuration.GetConnectionString("BlockbusterAppCon");
@@ -44,7 +41,88 @@ namespace BlockbusterAPI.Controllers
 
             return new JsonResult(table);
         }
-        
+
+        [HttpPost]
+        public JsonResult Post(Movie movie)
+        {
+            string query = @"
+                            INSERT INTO dbo.Movie
+                            (Title)
+                            VALUES (@Title)
+                            ";
+            DataTable table = new DataTable();
+            string dataSource = _configuration.GetConnectionString("BlockbusterAppCon");
+            SqlDataReader dataReader;
+            using (SqlConnection dbConnection = new SqlConnection(dataSource))
+            {
+                dbConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, dbConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@Title", movie.Title);
+                    dataReader = sqlCommand.ExecuteReader();
+                    table.Load(dataReader);
+                    dataReader.Close();
+                    dbConnection.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfuly");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Movie movie)
+        {
+            string query = @"
+                            UPDATE dbo.Movie
+                            SET Title = @Title
+                            WHERE Id = @Id
+                            ";
+            DataTable table = new DataTable();
+            string dataSource = _configuration.GetConnectionString("BlockbusterAppCon");
+            SqlDataReader dataReader;
+            using (SqlConnection dbConnection = new SqlConnection(dataSource))
+            {
+                dbConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, dbConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@Id", movie.Id);
+                    sqlCommand.Parameters.AddWithValue("@Title", movie.Title);
+                    dataReader = sqlCommand.ExecuteReader();
+                    table.Load(dataReader);
+                    dataReader.Close();
+                    dbConnection.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                            DELETE FROM dbo.Movie
+                            WHERE Id = @Id
+                            ";
+            DataTable table = new DataTable();
+            string dataSource = _configuration.GetConnectionString("BlockbusterAppCon");
+            SqlDataReader dataReader;
+            using (SqlConnection dbConnection = new SqlConnection(dataSource))
+            {
+                dbConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, dbConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@Id", id);
+                    dataReader = sqlCommand.ExecuteReader();
+                    table.Load(dataReader);
+                    dataReader.Close();
+                    dbConnection.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
         public IActionResult Index()
         {
              return View();
